92c7a3510ca54dc7c4dd8ffbd2f03eeb039d9693
d.aikema <djurreaikema@gmail.com>
2024-04-18 11:11:37 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angularstart-todo';
+  title: string = 'todo-test';
 }
