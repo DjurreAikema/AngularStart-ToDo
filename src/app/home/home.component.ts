8b58ab7af1eb6fa2750542dce2414102a064dbbf
d.aikema <djurreaikema@gmail.com>
2024-04-18 11:31:35 +0200

    Lesson 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
+import {TodoFormComponent} from "./ui/todo-form.component";
+import {Todo} from "../shared/interfaces/todo";
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  template: `<h2>Todo</h2>`,
+  template: `
+    <h2>Todo</h2>
+    <app-todo-form (todoSubmitted)="createTodo($event)"/>
+  `,
+  imports: [
+    TodoFormComponent
+  ]
 })
 export default class HomeComponent {
+
+  createTodo(todo: Todo) {
+    console.log(todo);
+  }
+
 }
---
+++ src/app/home/ui/todo-form.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, inject, Output} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Todo} from "../../shared/interfaces/todo";
+
+@Component({
+  standalone: true,
+  selector: 'app-todo-form',
+  template: `
+    <form [formGroup]="todoForm" (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())">
+      <input type="text" formControlName="title" placeholder="title..."/>
+      <input type="text" formControlName="description" placeholder="description..."/>
+      <button type="submit" [disabled]="!todoForm.valid">Add todo</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+export class TodoFormComponent {
+  @Output() todoSubmitted: EventEmitter<Todo> = new EventEmitter<Todo>();
+
+  private fb: FormBuilder = inject(FormBuilder);
+
+  todoForm = this.fb.nonNullable.group({
+    title: ['', Validators.required],
+    description: [''],
+  });
+}
